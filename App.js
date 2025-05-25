61ad16ae36619bbbdc2ce9df76c8065b41e3ff75
narasimha043 <vemulasimha08@gmail.com>
2025-05-25 13:59:34 +0530

    first commit

--- App.js
+++ App.js
@@ -1,6 +1,21 @@
 import React from 'react';
-import AppNavigator from './AppNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import SplashScreen from './src/screens/SplashScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import LoginScreen from './src/screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
-  return <AppNavigator />;
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SplashScreen" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="SplashScreen" component={SplashScreen} />
+        <Stack.Screen name="SignupScreen" component={SignupScreen} />
+        <Stack.Screen name="LoginScreen" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
--- AppNavigator.js
+++ AppNavigator.js
@@ -3,26 +3,32 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 // Import your screens from src/screens
-import HomeScreen from './src/screens/HomeScreen';
-import DetailsScreen from './src/screens/DetailsScreen';
+import SplashScreen from './src/screens/SplashScreen';
+import SignupScreen from './src/screens/SignupScreen';
+import LoginScreen from './src/screens/LoginScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AppNavigator() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
+      <Stack.Navigator initialRouteName="Splash">
         {/* Define your screens */}
         <Stack.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ title: 'Home' }}
+          name="Splash"
+          component={SplashScreen}
+          options={{ title: 'Spalsh' }}
         />
         <Stack.Screen
-          name="Details"
-          component={DetailsScreen}
-          options={{ title: 'Details' }}
+          name="Signup"
+          component={SignupScreen}
+          options={{ title: 'Signup' }}
         />
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: 'Login '}}
+          />
       </Stack.Navigator>
     </NavigationContainer>
   );
